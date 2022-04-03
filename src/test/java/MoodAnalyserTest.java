51128ff84eb90434bddde22c1b6a71ef314e78fd
Shubham <ssp671995@gmail.com>
2022-04-03 17:37:03 +0530

    Added ability to analyze happy or sad mood.

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertSame;
+
+/**
+ * create a class name as MoodAnalyserTest
+ */
 public class MoodAnalyserTest {
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        /**
+         * create object for  MoodAnalyserMain class
+         */
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        assertSame("HAPPY", mood);
+    }
 }
