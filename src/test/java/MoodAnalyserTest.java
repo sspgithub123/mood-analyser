f8dcecb83682584528b72f888373accd66cd867e
Shubham <ssp671995@gmail.com>
2022-04-03 18:15:39 +0530

    Added to inform used if entered invalid mood.

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -8,22 +8,25 @@ public class MoodAnalyserTest {
     @Test
     public void given_SadMood_Should_Return_SAD() {
         MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Sad Mood");
-        String mood = moodAnalyser.analyseMood();
-        assertSame("SAD", mood);
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            assertSame("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void given_AnyMood_Should_Return_HAPPY() {
         MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Happy Mood");
-        String mood = moodAnalyser.analyseMood();
-        assertSame("HAPPY", mood);
-    }
-
-    @Test
-    public void given_NULLMood_Should_Return_HAPPY() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain(null);
-        String mood = moodAnalyser.analyseMood();
-        assertSame("HAPPY", mood);
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            assertSame("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 }
 
