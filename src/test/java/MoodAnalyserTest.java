0e94295f5300920761426e26c5d4a32c8e52202e
Shubham <ssp671995@gmail.com>
2022-04-03 17:55:16 +0530

    Added I am in happy mood should return sad.

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -7,14 +7,15 @@ import static org.junit.Assert.assertSame;
 public class MoodAnalyserTest {
     @Test
     public void given_SadMood_Should_Return_SAD() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
         assertSame("SAD", mood);
     }
+
     @Test
     public void given_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Happy Mood");
+        String mood = moodAnalyser.analyseMood();
         assertSame("HAPPY", mood);
     }
 }
