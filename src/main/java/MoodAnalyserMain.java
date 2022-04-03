c2f9397bc7940591901b62f00c95d714bf7f46ab
Shubham <ssp671995@gmail.com>
2022-04-03 17:50:10 +0530

    Added I am in sad mood should return sad.

--- src/main/java/MoodAnalyserMain.java
+++ src/main/java/MoodAnalyserMain.java
@@ -9,14 +9,19 @@
  * create a class name as MoodAnalyserMain
  */
 public class MoodAnalyserMain {
+    private String message;
+
     /**
-     * create a constructor ,this is default constructor name as MoodAnalyserMain
+     * default constructor name as MoodAnalyserMain
      */
     public MoodAnalyserMain() {
     }
 
-    public String analyseMood(String message) {
-        if (message.contains("Sad"))
+    public MoodAnalyserMain(String message) {
+        this.message=message;
+    }
+    public String analyseMood() {
+        if (this.message.contains("Sad"))
             return "SAD";
         else
             return "HAPPY";
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -6,12 +6,9 @@ import static org.junit.Assert.assertSame;
  */
 public class MoodAnalyserTest {
     @Test
-    public void given_AnyMood_Should_Return_HAPPY() {
-        /**
-         * create a object for MoodAnalyserMain class
-         */
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+    public void given_SadMood_Should_Return_SAD() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Sad Mood");
+        String mood = moodAnalyser.analyseMood();
         assertSame("SAD", mood);
     }
 }
